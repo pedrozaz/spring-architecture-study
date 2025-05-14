7bca0eae49746824f7259df29f6521294e8fab0b
Guilherme <guilhermebarb0sa@proton.me>
2025-05-14 18:24:16 -0300

    Spring Architecture

---
+++ src/main/java/github/pedrozaz/springarchtecture/SpringArchtectureApplication.java
@@ -0,0 +1,13 @@
+package github.pedrozaz.springarchtecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArchtectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArchtectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/pedrozaz/springarchtecture/SpringArchtectureApplicationTests.java
@@ -0,0 +1,13 @@
+package github.pedrozaz.springarchtecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArchtectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
